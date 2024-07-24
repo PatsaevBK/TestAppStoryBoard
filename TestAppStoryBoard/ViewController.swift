8d5234319696932d281bf6bcac6bfd7c595dc152
Beknur Patsaev <patsaev1994@mail.ru>
2024-07-24 20:13:22 +0300

    initial

---
+++ TestAppStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppStoryBoard
+//
+//  Created by Beknur Patsaev on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
