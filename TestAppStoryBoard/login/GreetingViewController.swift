664831ebb38304044d48c6857ec608a480c897e7
Beknur Patsaev <patsaev1994@mail.ru>
2024-08-24 18:26:25 +0300

    Login screen teacher impl

--- TestAppStoryBoard/login/GreetingViewController.swift
+++ TestAppStoryBoard/login/GreetingViewController.swift
@@ -7,17 +7,40 @@
 
 import UIKit
 
-class GreetingViewController: UIViewController {
+final class GreetingViewController: UIViewController {
   
-  var userName: String? = nil
+  var userName: String!
   @IBOutlet weak var greeting: UILabel!
+  
+  private let primaryColor = UIColor(
+    red: 210/255,
+    green: 109/255,
+    blue: 128/255,
+    alpha: 1
+  )
+  private let secondaryColor = UIColor(
+    red: 107/255,
+    green: 148/255,
+    blue: 230/255,
+    alpha: 1
+  )
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
     greeting.text = "Welcome, \(userName ?? "")!"
   }
-  
-  @IBAction func tapOnLogOut(_ sender: Any) {
-    self.dismiss(animated: true)
+}
+
+// MARK: - Set background color
+extension UIView {
+  func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+    let gradient = CAGradientLayer()
+    gradient.frame = bounds
+    gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+    gradient.locations = [0.0, 1.0]
+    gradient.startPoint = CGPoint(x: 0, y: 0)
+    gradient.endPoint = CGPoint(x: 0, y: 1)
+    layer.insertSublayer(gradient, at: 0)
   }
 }
--- TestAppStoryBoard/login/LoginViewController.swift
+++ TestAppStoryBoard/login/LoginViewController.swift
@@ -8,27 +8,42 @@ final class LoginViewController: UIViewController {
   private let rightUserName = "User"
   private let rightPassword = "1"
   
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
+  
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     let controller = segue.destination as? GreetingViewController
     controller?.userName = userName.text
   }
   
-  private func checkNameAndPassword() -> Bool {
-    if let userName = userName.text, let password = password.text {
-      return (userName == rightUserName) && (password == rightPassword)
+  override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+    guard userName.text == rightUserName, password.text == rightPassword else {
+      showAlert(title: "Invalid password or login", message: "Please, enter correct login or password") {
+        self.password.text = ""
+      }
+      return false
     }
-    return false
+    return true
   }
   
-  @IBAction func tapOnLogin(_ sender: Any) {
-    if checkNameAndPassword() {
-      self.performSegue(withIdentifier: "ShowGreetings", sender: nil)
-    } else {
-      let alert = UIAlertController(title: "Invalid password or login", message: "Please, enter correct login or password", preferredStyle: .alert)
-      alert.addAction(UIAlertAction(title: "Ok", style: .destructive) {_ in 
-        self.password.text = ""
-      })
-      self.present(alert, animated: true)
+  @IBAction func unwind(for segue: UIStoryboardSegue) {
+    userName.text = ""
+    password.text = ""
+  }
+  
+  private func showAlert(title: String, message: String, actionOk: (() -> Void)? = nil) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+      actionOk?()
     }
+    alert.addAction(okAction)
+    present(alert, animated: true)
+  }
+  
+  @IBAction func forgotRegisterData(_ sender: UIButton) {
+    sender.tag == 0
+    ? showAlert(title: "Ooops", message: "Your name is \(rightUserName)")
+    : showAlert(title: "Ooops", message: "Your passwod is \(rightPassword)")
   }
 }
